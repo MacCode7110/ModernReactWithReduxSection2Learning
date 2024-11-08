5b968e876bda8998ed04f4d22ba9d16df46a3c08
Matthew McAlarney <memcalarney@wpi.edu>
2024-11-08 17:10:30 -0500

    Add style prop with border objectpassed in

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -7,15 +7,17 @@ const element = document.getElementById('root');
 const root = ReactDOM.createRoot(element);
 // 4 Create a component
 function App() {
-    const name = "Matthew"
-    const age = 23;
+    const inputType ="number";
+    const minValue = 5;
+    //const name = "Matthew"
+    //const age = 23;
     //let message = 'Bye there!'
     // if (Math.random() > 0.5) {
     //     message = 'Hello there!';
     // }
     //We do not always need to declare variables before returning values displayed in JSX in React: Could place the following in the h1 tag: new Date().toLocaleTimeString()
-    //We can place multiple variables in a return statement.
-    return <h1>Hi, my name is {name} and my age is {age}</h1>
+    //We can place multiple variables in a return statement: <h1>Hi, my name is {name} and my age is {age}</h1>
+    return <input style={{ border: '3px solid red' }} type={inputType} min={minValue}/>
 }
 // 5
 root.render(<App />)
