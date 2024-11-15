ffa96164a5e99515c3a9931caa8bbb8cd44a866f
Matthew McAlarney <memcalarney@wpi.edu>
2024-11-15 17:02:10 -0500

    Add styling

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -17,7 +17,11 @@ function App() {
     // }
     //We do not always need to declare variables before returning values displayed in JSX in React: Could place the following in the h1 tag: new Date().toLocaleTimeString()
     //We can place multiple variables in a return statement: <h1>Hi, my name is {name} and my age is {age}</h1>
-    return <input style={{ border: '3px solid red' }} type={inputType} min={minValue}/>
+    //<input style={{ border: '3px solid red' }} type={inputType} min={minValue}/>
+    //Could do maxLength = {5}
+    //Can also do <input spellCheck /> and <input spellCheck={false} >
+    //Style props in JSX need an object unlike regular HTML: { border: '1px solid blue'}
+    return <textarea style={{ border: '1px solid blue'}} autoFocus={true} maxLength = {5}/>;
 }
 // 5
 root.render(<App />)
